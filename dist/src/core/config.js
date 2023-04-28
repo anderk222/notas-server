7f6ed580d4b116da03e57bba2722a4a78b00c3dd
Anderson Macias <anderk222u@gmail.com>
2023-04-28 10:43:01 -0500

    remove dist folder

--- dist/src/core/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const APP = {
-    PORT: process.env.PORT || 4444
-};
-exports.APP = APP;
